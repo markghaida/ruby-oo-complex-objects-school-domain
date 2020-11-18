9a62d939c39bcbda6867b4f28d181668fff58b36
Mark Ghaida <markghaida@gmail.com>
2020-11-18 09:29:35 -0500

    commit tree bdda1d3609866f445d60232ae7ca840045dc995b

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
 # code here!
+require 'pry'
+class School 
+    attr_accessor :school, :student, :grade, :roster 
+
+    
+    def initialize(school)
+        @school = school 
+    end 
+
+
+    def roster
+        roster = {}
+    end 
+
+    def add_student(student, grade)
+        #binding.pry
+        @student = student  
+        @grade = grade 
+        
+        roster[grade] = []
+        roster[grade] << student 
+       end 
+   
+end 
